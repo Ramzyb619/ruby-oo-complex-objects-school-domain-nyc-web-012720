a9f0e1dfc967aef6362e28e73955cfd0d21faa0f
Ramzy Badawy <ramzy.badawy@gmail.com>
2020-02-04 00:11:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+class School 
+  def initialize (name, roster)
+    @name = name 
+    @roster = {}
+  end 
+  
+  def roster 
+    @roster
+  end 
+  
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end 
+end 
